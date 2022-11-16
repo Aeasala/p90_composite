cf3a0151c518a0559ff662edf5eab17edb0a07c9
Evan MacDonald <esmacdon@gmail.com>
2022-11-15 21:54:14 -0500

    fixed load errors, hook.add related

--- lua/autorun/gb5_help.lua
+++ lua/autorun/gb5_help.lua
@@ -2,7 +2,7 @@ AddCSLuaFile()
 
 
 if (CLIENT) then
-     function gbehelp( ply, text, public)
+     function gb5help( ply, text, public)
          if (string.find(text, "!gb5") != nil) then
 			 chat.AddText("Console commands:")
              chat.AddText("gb5_easyuse [0/1] - Should fireworks interact on use?")
@@ -12,5 +12,5 @@ if (CLIENT) then
 			 chat.AddText("gb5_explosion_damage  [0/1] - Should fireworks do damage upon explosion?")
 		 end
      end
+	 hook.Add( "OnPlayerChat", "gb5help", gb5help )
 end
-hook.Add( "OnPlayerChat", "gb5help", gb5help )
